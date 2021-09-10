a84cd35d20a6df3e515b5668789161dea7c98562
Sam Ban <sam.ban@icloud.com>
2021-09-10 23:35:56 +0100

    separate tests

--- backend/config_test.go
+++ backend/config_test.go
@@ -196,7 +196,54 @@ func TestGetGroups(t *testing.T) {
 	}
 }
 
-func TestUpdateGroups(t *testing.T) {
+func TestUpdateUserGroups(t *testing.T) {
+	cfg := testConfig("foo", map[string]Hostentry{
+		"hosta": {Alias: "hosta", Host: "a:22", User: "aroot", Groups: []string{"groupa", "groupb"}},
+		"hostb": {Alias: "hostb", Host: "b:22", User: "aroot", Groups: []string{"groupa", "groupc"}},
+	}, map[string]User{
+		"asdfasdf0": {Email: "foo@email", KeyType: "ssh-rsa", Key: "keydata", Name: "aroot", Groups: []string{"groupa", "groupb"}},
+		"asdfasdf1": {Email: "bar@email", KeyType: "ssh-rsa", Key: "keydata", Name: "broot", Groups: []string{"groupa", "groupc"}},
+	}, &SFTPConn{mock: true})
+	_, u := cfg.GetUserByEmail("foo@email")
+	if u == nil {
+		t.Errorf("error finding user by email")
+		return
+	}
+	old := u.Groups
+	u.Groups = []string{"groupa"}
+	u.UpdateGroups(cfg, old)
+	if contains(cfg.Hosts["hosta"].Users, "foo@email") {
+		t.Errorf("user is not on host")
+	}
+	if contains(cfg.Hosts["hostb"].Users, "foo@email") {
+		t.Errorf("user is still on host")
+	}
+	_, u = cfg.GetUserByEmail("bar@email")
+	if u == nil {
+		t.Errorf("error finding user by email")
+		return
+	}
+	old = u.Groups
+	u.Groups = []string{"groupb"}
+	u.UpdateGroups(cfg, old)
+	if contains(cfg.Hosts["hostb"].Users, "bar@email") {
+		t.Errorf("user is on wrong host")
+	}
+	if contains(cfg.Hosts["hosta"].Users, "bar@email") {
+		t.Errorf("user is still on host b")
+	}
+	old = u.Groups
+	u.Groups = []string{"groupb"}
+	u.UpdateGroups(cfg, old)
+	if contains(cfg.Hosts["hostb"].Users, "bar@email") {
+		t.Errorf("user is on wrong host")
+	}
+	if contains(cfg.Hosts["hosta"].Users, "bar@email") {
+		t.Errorf("user is still on host b")
+	}
+}
+
+func TestUpdateHostGroups(t *testing.T) {
 	cfg := testConfig("foo", map[string]Hostentry{
 		"hosta": {Alias: "hosta", Host: "a:22", User: "aroot", Groups: []string{"groupa", "groupb"}},
 		"hostb": {Alias: "hostb", Host: "b:22", User: "aroot", Groups: []string{"groupa", "groupc"}},
@@ -219,29 +266,6 @@ func TestUpdateGroups(t *testing.T) {
 	if u == nil {
 		t.Errorf("error finding user by email")
 		return
-	}
-	old = u.Groups
-	u.Groups = []string{"groupa"}
-	u.UpdateGroups(cfg, old)
-	if contains(cfg.Hosts["hosta"].Users, "foo@email") {
-		t.Errorf("user is not on host")
-	}
-	if contains(cfg.Hosts["hostb"].Users, "foo@email") {
-		t.Errorf("user is still on host")
-	}
-	_, u = cfg.GetUserByEmail("bar@email")
-	if u == nil {
-		t.Errorf("error finding user by email")
-		return
-	}
-	old = u.Groups
-	u.Groups = []string{"groupa"}
-	u.UpdateGroups(cfg, old)
-	if contains(cfg.Hosts["hostb"].Users, "bar@email") {
-		t.Errorf("user is on wrong host")
-	}
-	if contains(cfg.Hosts["hosta"].Users, "bar@email") {
-		t.Errorf("user is still on host b")
 	}
 }
 
