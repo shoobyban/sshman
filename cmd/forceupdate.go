9238a24d20559b44d051eafbed5346acb778d95b
Sam Ban <sam.ban@icloud.com>
2022-01-31 07:52:23 +0000

    removed some comments

--- cmd/forceupdate.go
+++ cmd/forceupdate.go
@@ -22,14 +22,4 @@ var forceupdateCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(forceupdateCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// forceupdateCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// forceupdateCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
