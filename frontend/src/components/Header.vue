82d6305b600e8b49de0079533fcbcddeca88cd6d
Sam Ban <sam@gpmd.co.uk>
2022-02-06 10:44:09 +0000

    frontend swag

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,9 +1,14 @@
 <template>
     <nav id="header" class="bg-white fixed w-full z-10 top-0 shadow">
-        <div class="w-full container mx-auto flex flex-wrap items-center mt-0 pt-3 pb-3 md:pb-0">
-            <div class="w-full pl-2 md:pl-0">
+        <div class="flex justify-between h-11 mt-0 pt-2">
+            <div class="ml-2 md:pl-0">
                 <a class="text-gray-900 text-base xl:text-xl no-underline hover:no-underline font-bold" href="#">
-                    <i class="fas fa-sun text-pink-600 pr-3 mb-4"></i> SSHMan
+                    <i class="fas fa-link text-pink-600 pr-3 mb-4"></i> SSHMan
+                </a>
+            </div>
+            <div class="mr-2">
+                <a href="https://github.com/shoobyban/sshman" target="new">
+                <img src="/github.png" alt="Github" class="h-8 w-8" />
                 </a>
             </div>
         </div>
