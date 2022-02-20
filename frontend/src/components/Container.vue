05e2abe783c893625d77bbbe8d57b06569fdb7d9
Sam Ban <sam@gpmd.co.uk>
2022-02-20 10:23:08 +0000

    switch for console

--- frontend/src/components/Container.vue
+++ frontend/src/components/Container.vue
@@ -9,11 +9,14 @@ export default {
     },
     data() {
         return {
-            title: 'Container'
+            title: 'Container',
+            console: true,
         }
     },
     mounted() {
         this.$store.dispatch("initTheme")
+        const noConsole = window.localStorage.getItem('console')=='false'
+        this.console = !noConsole
         this.refreshTheme()
     },
     methods: {
@@ -27,19 +30,24 @@ export default {
                 document.getElementById('theme-toggle-light-icon').classList.add('hidden')
                 document.body.classList.remove('dark')
             }
-        }
+        },
+        toggleConsole() {
+            console.log('toggle console', this.console)
+            this.console = !this.console
+            window.localStorage.setItem('console', this.console)
+        },
     }
 }
 </script>
 
 <template>
     <div class="flex flex-col h-screen">
-        <Header />
+        <Header :console="console" @toggle-console="toggleConsole" />
         <div class="flex flex-grow overflow-hidden dark:bg-gray-900 dark:text-white bg-white pt-16">
             <div class="w-full bg-gray-50 dark:bg-gray-900 dark:text-white relative overflow-y-auto p-5">
                 <router-view />
             </div>
-            <div class="bg-gray-100 max-h-full dark:bg-gray-900 dark:text-white text-blue-100 md:w-80 pa-2 md:relative">
+            <div v-show="console" class="bg-gray-100 max-h-full dark:bg-gray-900 dark:text-white text-blue-100 md:w-80 pa-2 md:relative">
                 <h2 class="text-black dark:text-white text-center mb-2 mt-2 font-bold">
                     <span>Console</span>
                 </h2>
